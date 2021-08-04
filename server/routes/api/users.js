4dc65240627915f0be829186487a963936659eb5
Konrad Goldammer <konradgoldammer@gmail.com>
2021-08-04 14:08:32 +0200

    Update register route

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -7,47 +7,66 @@ const config = require("config");
 const jwt = require("jsonwebtoken");
 const auth = require("../../middleware/auth");
 
+const isValidEmail = (email) => {
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email.match(regexEmail)) {
+    return true;
+  }
+  return false;
+};
+
 // @route    POST "/"
 // @desc.    Register new user
 // @access   Public
 router.post("/", (req, res) => {
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
+  const name = req.body.name.toLowerCase();
 
   // Simple validation
   if (!name || !email || !password) {
     return res.status(400).json({ msg: "Please enter all fields" });
   }
 
-  User.findOne({ email }).then((user) => {
+  if (!isValidEmail(email)) {
+    return res.status(400).json({ msg: "Please enter a valid email" });
+  }
+
+  User.findOne({ name }).then((user) => {
     if (user) {
-      return res
-        .status(400)
-        .json({ msg: "User with that email already exists" });
+      return res.status(400).json({ msg: "Username is already taken" });
     }
 
-    const newUser = { name, email, password };
-
-    // Create hashed password
-    bcrypt.hash(newUser.password, 10, (err, hashedPassword) => {
-      if (err) {
-        throw err;
+    User.findOne({ email }).then((user) => {
+      if (user) {
+        return res
+          .status(400)
+          .json({ msg: "User with that email already exists" });
       }
-      newUser.password = hashedPassword;
-      new User(newUser).save().then((user) => {
-        jwt.sign(
-          { id: user.id },
-          config.get("jwtSecret"),
-          { expiresIn: 3600 },
-          (err, token) => {
-            if (err) {
-              throw err;
+
+      const newUser = { name, email, password };
+
+      // Create hashed password
+      bcrypt.hash(newUser.password, 10, (err, hashedPassword) => {
+        if (err) {
+          throw err;
+        }
+        newUser.password = hashedPassword;
+        new User(newUser).save().then((user) => {
+          jwt.sign(
+            { id: user.id },
+            config.get("jwtSecret"),
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) {
+                throw err;
+              }
+              res.json({
+                token,
+                user: { id: user.id, name: user.name, email: user.email },
+              });
             }
-            res.json({
-              token,
-              user: { id: user.id, name: user.name, email: user.email },
-            });
-          }
-        );
+          );
+        });
       });
     });
   });
