3f708ffa41f6a19b297a74b9ecdff4c67db962bd
Konrad Goldammer <konradgoldammer@gmail.com>
2021-08-08 14:58:54 +0200

    Add like/unlike episode feature

--- server/routes/api/episodes.js
+++ server/routes/api/episodes.js
@@ -132,4 +132,130 @@ router.put("/unlisten", auth, (req, res) => {
     });
 });
 
+// @route    PUT "/like"
+// @desc.    Like an episode
+// @access   Private
+router.put("/like", auth, (req, res) => {
+  const { episodeId } = req.body;
+
+  // Simple validation
+  if (!episodeId) {
+    return res.status(400).json({ msg: "Did not receive episode ID" });
+  }
+
+  // Get user to check if he already marked this episode as listened
+  User.findById(req.user.id).then((user) => {
+    const listenedEpisode = user.episodes.filter((item) => {
+      return item.episode.id === episodeId;
+    })[0];
+
+    // Checks if episodes array includes episode
+    if (!listenedEpisode) {
+      // Fetch episode data
+      client
+        .fetchEpisodeById({ id: episodeId })
+        .then((response) => {
+          const episode = {
+            id: response.data.id,
+            title: response.data.title,
+            date: new Date(response.data.pub_date_ms),
+            thumbnail: response.data.thumbnail,
+            podcast: {
+              id: response.data.podcast.id,
+              title: response.data.podcast.title,
+            },
+          };
+
+          // Add episode to user's episodes array
+          User.findOneAndUpdate(
+            { _id: req.user.id },
+            {
+              $push: {
+                episodes: { episode, like: true, episodeId: episode.id },
+              },
+            },
+            { new: true }
+          )
+            .then((user) => {
+              return res.json(user);
+            })
+            .catch((err) => {
+              return console.log(err);
+            });
+        })
+        .catch(() => {
+          return res
+            .status(404)
+            .json({ msg: `Couldn't find episode with the ID ${episodeId}` });
+        });
+    } else if (listenedEpisode) {
+      // Check if user has already liked this episode
+      if (listenedEpisode.like) {
+        return res
+          .status(400)
+          .json({ msg: "You've already liked this episode" });
+      }
+
+      // Set the like property to true of the according item in the episodes array of the user
+      User.findOneAndUpdate(
+        { _id: req.user.id, "episodes.episodeId": episodeId },
+        { $set: { "episodes.$.like": true } },
+        { new: true }
+      )
+        .then((user) => {
+          return res.json(user);
+        })
+        .catch((err) => {
+          return console.log(err);
+        });
+    }
+  });
+});
+
+// @route    PUT "/unlike"
+// @desc.    Unlike an episode
+// @access   Private
+router.put("/unlike", auth, (req, res) => {
+  const { episodeId } = req.body;
+
+  // Simple validation
+  if (!episodeId) {
+    return res.status(400).json({ msg: "Did not receive episode ID" });
+  }
+
+  // Get user to check if he already marked this episode as listened
+  User.findById(req.user.id).then((user) => {
+    const listenedEpisode = user.episodes.filter((item) => {
+      return item.episode.id === episodeId;
+    })[0];
+
+    // Checks if episodes array includes episode
+    if (!listenedEpisode) {
+      return res.status(400).json({
+        msg: "You haven't liked this episode, therefore you can't unlike it",
+      });
+    }
+
+    // Check if user has already not liked the episode
+    if (!listenedEpisode.like) {
+      return res.status(400).json({
+        msg: "You haven't liked the episode, therefore you can't unlike it",
+      });
+    }
+
+    // Set the like property to false of the according item in the episodes array of the user
+    User.findOneAndUpdate(
+      { _id: req.user.id, "episodes.episodeId": episodeId },
+      { $set: { "episodes.$.like": false } },
+      { new: true }
+    )
+      .then((user) => {
+        return res.json(user);
+      })
+      .catch((err) => {
+        return console.log(err);
+      });
+  });
+});
+
 module.exports = router;
