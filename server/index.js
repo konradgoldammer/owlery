ed70f2d85404fb3f2880e0656cf511b369d70e6f
Konrad Goldammer <konradgoldammer@gmail.com>
2021-09-04 00:21:41 +0200

    Remove no longer supported mongo options

--- server/index.js
+++ server/index.js
@@ -8,8 +8,6 @@ mongoose
   .connect(config.get("mongoURI"), {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   })
   .then(console.log("MongoDB connected..."))
   .catch((err) => {
