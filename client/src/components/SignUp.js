f66430bb9696e77d446014de3e3cc5efb1546c9c
Duarte Almeida <duartemalmeida@gmail.com>
2021-09-04 11:59:00 +0100

    Finish building form structure

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 const SignUp = () => {
-  const [email, setEmail] = useState();
-  const [username, setUsername] = useState(null);
-  const [password, setPassword] = useState();
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // dispatch()
-  };
-
-  const handleChange = (e) => {
-    //catch values
+    const userInfo = { email, username, password };
+    setEmail("");
+    setUsername("");
+    setPassword("");
+    setConfirmPassword("");
   };
 
   return (
-    <div className="container">
+    <div className="signup-container">
+      <h2 className="signup-title">owlery</h2>
       <Form onSubmit={handleSubmit}>
         <FormGroup row>
           <Label for="email">Email address</Label>
@@ -25,30 +29,67 @@ const SignUp = () => {
               type="email"
               name="email"
               id="email"
+              required
               value={email}
-              onChange={handleChange}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </Col>
         </FormGroup>
         <FormGroup row>
           <Label for="username">Username</Label>
           <Col sm={2}>
-            <Input type="text" name="user" id="user" />
+            <Input
+              type="text"
+              name="user"
+              id="user"
+              required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </Col>
         </FormGroup>
         <FormGroup row>
           <Label for="password">Password</Label>
           <Col sm={2}>
-            <Input type="password" name="password" id="password" />
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </Col>
         </FormGroup>
         <FormGroup row>
           <Label for="confirmPassword">Confirm password</Label>
           <Col sm={2}>
-            <Input type="password" name="confirmPass" id="confirmPass" />
+            <Input
+              type="password"
+              name="confirmPass"
+              id="confirmPass"
+              required
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
           </Col>
         </FormGroup>
+        <FormGroup check>
+          <Label check>
+            <Input type="checkbox" />I accept the{" "}
+            <Link to="">Terms of Use</Link>
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+          <Label check>
+            <Input type="checkbox" />I accept the{" "}
+            <Link to="">Privacy Policy</Link>
+          </Label>
+        </FormGroup>
         <Button color="warning">Create Account</Button>
+        <p>
+          Already have an account? <Link to="">Sign In.</Link>
+        </p>
       </Form>
     </div>
   );
