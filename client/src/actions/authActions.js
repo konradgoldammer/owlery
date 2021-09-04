3077192881017637f1d6466bf377d8d9c4fa4b31
Konrad Goldammer <konradgoldammer@gmail.com>
2021-09-04 14:28:13 +0200

    Add register action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -25,6 +25,29 @@ export const loadUser = () => (dispatch, getState) => {
     });
 };
 
+// Register User
+export const register = (username, email, password) => (dispatch) => {
+  //Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  // Request body
+  const body = JSON.stringify({ username, email, password });
+
+  axios
+    .post("api/users", body, config)
+    .then((res) => dispatch({ type: REGISTER_SUCCESS, payload: res.data }))
+    .catch((err) => {
+      dispatch(
+        returnErrors(err.response.data, err.response.status, "REGISTER_FAIL")
+      );
+      dispatch({ type: REGISTER_FAIL });
+    });
+};
+
 // Setup config/headers and token
 export const tokenConfig = (getState) => {
   // Get token from localstorage
