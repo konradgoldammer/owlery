204a359cc229151acd56d6e18f8c2184c65952a1
Duarte Almeida <duartemalmeida@gmail.com>
2021-09-05 16:58:43 +0100

    Add classnames and styles to form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -28,78 +28,96 @@ const SignUp = () => {
     setConfirmPassword("");
   };
 
+  // style for link tags
+
+  const tagStyle = {
+    textDecoration: "none",
+    color: "#7EB600",
+  };
+
   return (
     <div className="signup-container">
       <Form className="signUpForm" onSubmit={handleSubmit}>
-        <h2 className="signup-title">owlery</h2>
-        <FormGroup row>
-          <Label for="email">Email address</Label>
-          <Col sm={2}>
-            <Input
-              type="email"
-              name="email"
-              id="email"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="username">Username</Label>
-          <Col sm={2}>
-            <Input
-              type="text"
-              name="user"
-              id="user"
-              required
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="password">Password</Label>
-          <Col sm={2}>
-            <Input
-              type="password"
-              name="password"
-              id="password"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="confirmPassword">Confirm password</Label>
-          <Col sm={2}>
-            <Input
-              type="password"
-              name="confirmPass"
-              id="confirmPass"
-              required
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" />I accept the{" "}
-            <Link to="">Terms of Use</Link>
-          </Label>
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" />I accept the{" "}
-            <Link to="">Privacy Policy</Link>
-          </Label>
-        </FormGroup>
-        <Button color="warning">Create Account</Button>
-        <p>
-          Already have an account? <Link to="">Sign In.</Link>
-        </p>
+        <h2 className="signUpTitle">owlery</h2>
+        <div className="insideForm">
+          <FormGroup row>
+            <Label for="email">Email address</Label>
+            <Col sm={8}>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="username">Username</Label>
+            <Col sm={8}>
+              <Input
+                type="text"
+                name="user"
+                id="user"
+                required
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="password">Password</Label>
+            <Col sm={8}>
+              <Input
+                type="password"
+                name="password"
+                id="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="confirmPassword">Confirm password</Label>
+            <Col sm={8}>
+              <Input
+                type="password"
+                name="confirmPass"
+                id="confirmPass"
+                required
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup check>
+            <Label check for="chk">
+              <Input type="checkbox" />I accept the{" "}
+              <Link to="" style={tagStyle}>
+                Terms of Use
+              </Link>
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Label check for="chk">
+              <Input type="checkbox" />I accept the{" "}
+              <Link to="" style={tagStyle}>
+                Privacy Policy
+              </Link>
+            </Label>
+          </FormGroup>
+          <Button className="signupBtn" color="warning">
+            Create Account
+          </Button>
+          <p className="createAccount">
+            Already have an account?{" "}
+            <Link to="" style={tagStyle}>
+              Sign In.
+            </Link>
+          </p>
+        </div>
       </Form>
       {error.id === "REGISTER_FAIL" && (
         <Alert color="danger">{error.msg}</Alert>
