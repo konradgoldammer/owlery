77b1bbbf2344eafc0d0fb2a04a48dc984153d88b
Konrad Goldammer <konradgoldammer@gmail.com>
2021-09-06 19:25:50 +0200

    Fix Bootstrap implementation

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./css/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/style.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
