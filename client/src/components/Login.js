a7bb2d5bed27d79848500588e94ed7dd1c5a5c79
Duarte Almeida <duartemalmeida@gmail.com>
2021-09-06 19:31:08 +0100

    Create Login component

---
+++ client/src/components/Login.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Button, Form, FormGroup, Label, Input, Col, Alert } from "reactstrap";
+import store from "../store";
+import { useSelector } from "react-redux";
+import "../App.css";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const error = useSelector((state) => state.error);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="login-container">
+      <Form className="loginForm" onSubmit={handleLogin}>
+        <h2 className="loginTitle">owlery</h2>
+        <div className="insideLoginForm">
+          <FormGroup row>
+            <Label for="username">Username or email address</Label>
+            <Col sm={12}>
+              <Input
+                type="text"
+                name="username"
+                id="username"
+                required
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="password">
+              Password
+              <Link className="forgotPass" to="/">
+                Forgot Password
+              </Link>
+            </Label>
+            <Col sm={12}>
+              <Input
+                type="password"
+                name="password"
+                id="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Col>
+          </FormGroup>
+          <Button className="loginBtn btn btn-sm" color="primary">
+            Sign In
+          </Button>
+          <p className="createAccount">
+            New to owlery?{" "}
+            <Link to="/login" className="createAccount">
+              Create an account.
+            </Link>
+          </p>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
