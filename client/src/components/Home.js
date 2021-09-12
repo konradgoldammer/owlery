e300d3b07d10cce63ea4a37461a0f014282094c3
Resul Selmani <resulselmani2002@gmail.com>
2021-09-12 23:57:40 +0200

    Adding Profile Picture

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MainNavbar from "./subcomponents/MainNavbar";
 import "../App.css";
+import ProfileInfo from "./ProfileInfo";
 
 const Home = () => {
   return (
     <div>
       <MainNavbar />
+      <ProfileInfo />
     </div>
   );
 };
---
+++ client/src/components/ProfileInfo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import profilePic from "./images/profile.png";
+import "../App.css";
+import { Container } from "reactstrap";
+import { Row } from "reactstrap";
+
+function ProfileInfo() {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <img src={profilePic} alt="profilepicture" className="profile-pic" />
+          <h4>Konrad Goldammer</h4>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ProfileInfo;
