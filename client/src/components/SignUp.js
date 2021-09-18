abc51cbb089117a5742bc1fe17dfd47336e885e4
Konrad Goldammer <konradgoldammer@gmail.com>
2021-09-18 16:11:30 +0200

    Implement validation for the data of the form in the 'SignUp' component

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Button, Form, FormGroup, Label, Input, Col, Alert } from "reactstrap";
 import { Link, Redirect } from "react-router-dom";
 import { register } from "../actions/authActions";
-import { returnErrors, clearErrors } from "../actions/errorActions";
+import { clearErrors } from "../actions/errorActions";
 import { useSelector } from "react-redux";
 import SmallFooter from "./subcomponents/SmallFooter";
 import store from "../store";
@@ -23,6 +23,11 @@ const SignUp = () => {
     initialState.confirmPassword
   );
 
+  const [emailError, setEmailError] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
   const error = useSelector((state) => state.error);
   const auth = useSelector((state) => state.auth);
 
@@ -41,25 +46,129 @@ const SignUp = () => {
     );
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  // Email validation
+  const validateEmail = (email) => {
+    const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!email) {
+      setEmailError("'Email' field cannot be empty");
+      return true;
+    }
+
+    if (email.length > 50) {
+      setEmailError("Your email cannot be longer than 50 characters");
+      return true;
+    }
+
+    if (!email.match(regexEmail)) {
+      setEmailError("Please enter a valid email");
+      return true;
+    }
+
+    return false;
+  };
+
+  // Username validation
+  const validateUsername = (username) => {
+    const regexUsername = /^(\w|\.|-)+$/;
+
+    if (!username) {
+      setUsernameError("'Username' field cannot be empty");
+      return true;
+    }
+
+    if (username.length < 3) {
+      setUsernameError("Your username has to have at least 3 characters");
+      return true;
+    }
+
+    if (username.length > 30) {
+      setUsernameError("Your username cannot be longer than 30 characters");
+      return true;
+    }
+
+    if (!username.match(regexUsername)) {
+      setUsernameError(
+        "Your username can only include letters, numbers, dots, underscores or dashes"
+      );
+      return true;
+    }
+
+    if (username.toUpperCase() === username.toLowerCase()) {
+      setUsernameError("Your username has to include at least 1 letter");
+      return true;
+    }
+
+    return false;
+  };
+
+  // Password validation
+  const validatePassword = (password) => {
+    if (!password) {
+      setPasswordError("'Password' field cannot be empty");
+      return true;
+    }
+
+    if (password.length < 6) {
+      setPasswordError("Your password has to have at least 6 characters");
+      return true;
+    }
+
+    if (password.length > 50) {
+      setPasswordError("Your password cannot be longer than 50 characters");
+      return true;
+    }
+
+    return false;
+  };
+
+  // Confirm password validation
+  const validateConfirmPassword = (confirmPassword, password) => {
+    if (!confirmPassword) {
+      setConfirmPasswordError("'Confirm password' field cannot be empty");
+      return true;
+    }
 
     // Check if password and password confirmation match
     if (password !== confirmPassword) {
-      return store.dispatch(
-        returnErrors(
-          "Password and the password confirmation do not match",
-          400,
-          "REGISTER_FAIL"
-        )
+      setConfirmPasswordError(
+        "'Confirm password' field does not match with the 'Password' field"
       );
+      return true;
     }
 
-    // Clear form
-    setEmail(initialState.email);
-    setUsername(initialState.username);
-    setPassword(initialState.password);
-    setConfirmPassword(initialState.confirmPassword);
+    return false;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate email
+    const emailIsInvalid = validateEmail(email);
+
+    // Validate username
+    const usernameIsInvalid = validateUsername(username);
+
+    // Validate password
+    const passwordIsInvalid = validatePassword(password);
+
+    // Validate confirm password
+    const confirmPasswordIsInvalid = validateConfirmPassword(
+      confirmPassword,
+      password
+    );
+
+    // Clear errors
+    store.dispatch(clearErrors());
+
+    if (
+      emailIsInvalid ||
+      usernameIsInvalid ||
+      passwordIsInvalid ||
+      confirmPasswordIsInvalid
+    ) {
+      return;
+    }
 
     // Create new user object
     const newUser = { username, email, password };
@@ -79,9 +188,13 @@ const SignUp = () => {
                 type="email"
                 name="email"
                 id="email"
+                className={emailError && "input-error"}
                 required
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  setEmailError("");
+                }}
               />
             </Col>
           </FormGroup>
@@ -92,9 +205,13 @@ const SignUp = () => {
                 type="text"
                 name="user"
                 id="user"
+                className={usernameError && "input-error"}
                 required
                 value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                  setUsernameError("");
+                }}
               />
             </Col>
           </FormGroup>
@@ -105,9 +222,13 @@ const SignUp = () => {
                 type="password"
                 name="password"
                 id="password"
+                className={passwordError && "input-error"}
                 required
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                  setPasswordError("");
+                }}
               />
             </Col>
           </FormGroup>
@@ -118,14 +239,40 @@ const SignUp = () => {
                 type="password"
                 name="confirmPass"
                 id="confirmPass"
+                className={confirmPasswordError && "input-error"}
                 required
                 value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                  setConfirmPasswordError("");
+                }}
               />
             </Col>
           </FormGroup>
           {error.id === "REGISTER_FAIL" && (
-            <Alert color="danger p-2 error-msg-alert">{error.msg}</Alert>
+            <Alert color="danger" className="p-2 mt-1 mb-0 error-msg-alert">
+              {error.msg}
+            </Alert>
+          )}
+          {emailError && (
+            <Alert color="danger" className="p-2 mt-1 mb-0 error-msg-alert">
+              {emailError}
+            </Alert>
+          )}
+          {usernameError && (
+            <Alert color="danger" className="p-2 mt-1 mb-0 error-msg-alert">
+              {usernameError}
+            </Alert>
+          )}
+          {passwordError && (
+            <Alert color="danger" className="p-2 mt-1 mb-0 error-msg-alert">
+              {passwordError}
+            </Alert>
+          )}
+          {confirmPasswordError && (
+            <Alert color="danger" className="p-2 mt-1 mb-0 error-msg-alert">
+              {confirmPasswordError}
+            </Alert>
           )}
           <FormGroup check className="mt-3">
             <Label check for="chk">
