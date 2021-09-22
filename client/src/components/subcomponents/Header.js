008a9dc9f616399d5d5fa2bcbfb563a6f255914c
Konrad Goldammer <konradgoldammer@gmail.com>
2021-09-22 22:32:29 +0200

    Add heading-font-family variable

--- client/src/components/subcomponents/Header.js
+++ client/src/components/subcomponents/Header.js
@@ -3,11 +3,13 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <h2 className="header-text">
-        Track the <mark>podcasts</mark> you&apos;ve heard. <br />
-        Save those you want to hear. <br />
-        Tell your friends what&apos;s good.
-      </h2>
+      <div className="container">
+        <h2 className="header-text">
+          Track the <mark>podcasts</mark> you&apos;ve heard. <br />
+          Save those you want to hear. <br />
+          Tell your friends what&apos;s good.
+        </h2>
+      </div>
     </div>
   );
 };
