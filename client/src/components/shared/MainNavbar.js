0911f69d23aaf9f2a6c67c9df84beac5073d1926
Konrad Goldammer <konradgoldammer@gmail.com>
2021-09-26 14:19:26 +0200

    Add container around 'MainNavbar'

--- client/src/components/shared/MainNavbar.js
+++ client/src/components/shared/MainNavbar.js
@@ -32,95 +32,100 @@ const MainNavbar = () => {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div className="fixed-top">
-      <Navbar color="dark" dark expand="md" className="px-4">
-        <NavbarBrand href="/" className="navbar-brand text-lowercase">
-          Owlery
-        </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/podcasts" className="text-uppercase navbar-link">
-                <strong>Podcasts</strong>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/lists" className="text-uppercase navbar-link">
-                <strong>Lists</strong>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/members" className="text-uppercase navbar-link">
-                <strong>Members</strong>
-              </NavLink>
-            </NavItem>
-          </Nav>
-
-          {auth.isAuthenticated ? (
-            <UncontrolledDropdown
-              nav
-              inNavbar
-              className="d-flex flex-row-reverse w-100"
-            >
-              <DropdownToggle
-                nav
-                caret
-                className="text-uppercase profile-dropdown-toggle"
-              >
-                <strong>{auth.user.username}</strong>
-              </DropdownToggle>
-              <DropdownMenu right className="profile-dropdown-menu">
-                <DropdownItem className="profile-dropdown-item text-uppercase">
-                  <strong>Profile</strong>
-                </DropdownItem>
-                <DropdownItem className="profile-dropdown-item text-uppercase">
-                  <strong>Settings</strong>
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem
-                  className="profile-dropdown-item text-uppercase"
-                  onClick={() => store.dispatch(logout())}
+    <div className="main-navbar fixed-top">
+      <div className="container-md">
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/" className="navbar-brand text-lowercase">
+            Owlery
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink
+                  href="/podcasts"
+                  className="text-uppercase navbar-link"
                 >
-                  <strong>Logout</strong>
-                </DropdownItem>{" "}
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          ) : (
-            <div className="d-flex flex-row-reverse d-none d-md-flex w-100">
-              <Link to="/create-account">
-                <button className="btn btn-sm btn-primary text-uppercase">
-                  <strong>Create Account</strong>
-                </button>
-              </Link>
-              <Link to="/login">
-                <button className="btn btn-sm btn-dark text-uppercase mx-2">
-                  <strong>Login</strong>
-                </button>
-              </Link>
+                  <strong>Podcasts</strong>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/lists" className="text-uppercase navbar-link">
+                  <strong>Lists</strong>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/members" className="text-uppercase navbar-link">
+                  <strong>Members</strong>
+                </NavLink>
+              </NavItem>
+            </Nav>
+
+            {auth.isAuthenticated ? (
+              <UncontrolledDropdown
+                nav
+                inNavbar
+                className="d-flex flex-row-reverse w-100"
+              >
+                <DropdownToggle
+                  nav
+                  caret
+                  className="text-uppercase profile-dropdown-toggle"
+                >
+                  <strong>{auth.user.username}</strong>
+                </DropdownToggle>
+                <DropdownMenu right className="profile-dropdown-menu">
+                  <DropdownItem className="profile-dropdown-item text-uppercase">
+                    <strong>Profile</strong>
+                  </DropdownItem>
+                  <DropdownItem className="profile-dropdown-item text-uppercase">
+                    <strong>Settings</strong>
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem
+                    className="profile-dropdown-item text-uppercase"
+                    onClick={() => store.dispatch(logout())}
+                  >
+                    <strong>Logout</strong>
+                  </DropdownItem>{" "}
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            ) : (
+              <div className="d-flex flex-row-reverse d-none d-md-flex w-100">
+                <Link to="/create-account">
+                  <button className="btn btn-sm btn-primary text-uppercase">
+                    <strong>Create Account</strong>
+                  </button>
+                </Link>
+                <Link to="/login">
+                  <button className="btn btn-sm btn-dark text-uppercase mx-2">
+                    <strong>Login</strong>
+                  </button>
+                </Link>
+              </div>
+            )}
+          </Collapse>
+
+          {!isOpen && (
+            <div className="input-group searchbar">
+              <input
+                className="form-control my-0 py-1 searchbar-input"
+                type="text"
+                placeholder="Search"
+                aria-label="Search"
+                onChange={changeSearchInput}
+              />
+              <div>
+                <a href="/">
+                  <span className="input-group-text searchbar-span">
+                    <FaSearch className="searchbar-icon"></FaSearch>
+                  </span>
+                </a>
+              </div>
             </div>
           )}
-        </Collapse>
-
-        {!isOpen && (
-          <div className="input-group searchbar">
-            <input
-              className="form-control my-0 py-1 searchbar-input"
-              type="text"
-              placeholder="Search"
-              aria-label="Search"
-              onChange={changeSearchInput}
-            />
-            <div>
-              <a href="/">
-                <span className="input-group-text searchbar-span">
-                  <FaSearch className="searchbar-icon"></FaSearch>
-                </span>
-              </a>
-            </div>
-          </div>
-        )}
-      </Navbar>
+        </Navbar>
+      </div>
     </div>
   );
 };
