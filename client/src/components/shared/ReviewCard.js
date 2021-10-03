2f0efb2f839939d707f21a06b89f52cc47ebfbbe
Konrad Goldammer <konradgoldammer@gmail.com>
2021-10-03 22:07:35 +0200

    Relocate 'Link' in 'ReviewCard'

--- client/src/components/shared/ReviewCard.js
+++ client/src/components/shared/ReviewCard.js
@@ -15,109 +15,109 @@ const ReviewCard = (props) => {
   const [showMore, setShowMore] = useState(false);
 
   return (
-    <Link to={`/review/${review._id}`} className="text-decoration-none">
-      <div className="review-card w-100 p-3 mb-2" title={review.episode.title}>
-        <div className="row">
-          <div className="col-3">
-            <img
-              src={review.episode.thumbnail}
-              alt=""
-              className="review-card-image w-100"
-            />
-          </div>
-          <div className="col-9">
+    <div className="review-card w-100 p-3 mb-2" title={review.episode.title}>
+      <div className="row">
+        <div className="col-3">
+          <img
+            src={review.episode.thumbnail}
+            alt=""
+            className="review-card-image w-100"
+          />
+        </div>
+        <div className="col-9">
+          <Link to={`/review/${review._id}`} className="text-decoration-none">
             <h5 className="review-card-title mb-0">{review.episode.title}</h5>
-            <div className="review-card-info mt-1">
-              <Link className="d-inline text-decoration-none">
-                <p className="review-card-author d-inline">
-                  <FaUserCircle className="me-1" />
-                  {review.author.username}
-                </p>
-              </Link>
-              {rating !== -1 && (
-                <div className="row d-inline ms-2">
-                  <div className="col d-inline p-0">
-                    {rating <= 0 ? (
-                      <BsStar />
-                    ) : rating === 1 ? (
-                      <BsStarHalf />
-                    ) : (
-                      <BsStarFill />
-                    )}
-                    {rating <= 2 ? (
-                      <BsStar />
-                    ) : rating === 3 ? (
-                      <BsStarHalf />
-                    ) : (
-                      <BsStarFill />
-                    )}
-                    {rating <= 4 ? (
-                      <BsStar />
-                    ) : rating === 5 ? (
-                      <BsStarHalf />
-                    ) : (
-                      <BsStarFill />
-                    )}
-                    {rating <= 6 ? (
-                      <BsStar />
-                    ) : rating === 7 ? (
-                      <BsStarHalf />
-                    ) : (
-                      <BsStarFill />
-                    )}
-                    {rating <= 8 ? (
-                      <BsStar />
-                    ) : rating === 9 ? (
-                      <BsStarHalf />
-                    ) : (
-                      <BsStarFill />
-                    )}
-                  </div>
+          </Link>
+          <div className="review-card-info mt-1">
+            <Link className="d-inline text-decoration-none">
+              <p className="review-card-author d-inline">
+                <FaUserCircle className="me-1" />
+                {review.author.username}
+              </p>
+            </Link>
+            {rating !== -1 && (
+              <div className="row d-inline ms-2">
+                <div className="col d-inline p-0">
+                  {rating <= 0 ? (
+                    <BsStar />
+                  ) : rating === 1 ? (
+                    <BsStarHalf />
+                  ) : (
+                    <BsStarFill />
+                  )}
+                  {rating <= 2 ? (
+                    <BsStar />
+                  ) : rating === 3 ? (
+                    <BsStarHalf />
+                  ) : (
+                    <BsStarFill />
+                  )}
+                  {rating <= 4 ? (
+                    <BsStar />
+                  ) : rating === 5 ? (
+                    <BsStarHalf />
+                  ) : (
+                    <BsStarFill />
+                  )}
+                  {rating <= 6 ? (
+                    <BsStar />
+                  ) : rating === 7 ? (
+                    <BsStarHalf />
+                  ) : (
+                    <BsStarFill />
+                  )}
+                  {rating <= 8 ? (
+                    <BsStar />
+                  ) : rating === 9 ? (
+                    <BsStarHalf />
+                  ) : (
+                    <BsStarFill />
+                  )}
                 </div>
-              )}
-              {like && (
-                <p className="d-inline ms-2">
-                  <FaHeart />
-                </p>
-              )}
-            </div>
-            <p className="mt-2 review-card-content">
-              {showMore
-                ? review.content
-                : review.content.substring(0, maxReviewContentLength)}
-              {review.content.length > maxReviewContentLength &&
-                !showMore &&
-                "..."}
-              {review.content.length > maxReviewContentLength && (
-                <button
-                  className="review-show-more-button bg-transparent"
-                  title={showMore ? "show less" : "show more"}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setShowMore(!showMore);
-                  }}
-                >
-                  {showMore ? "show less" : "show more"}
-                </button>
-              )}
+              </div>
+            )}
+            {like && (
+              <p className="d-inline ms-2">
+                <FaHeart />
+              </p>
+            )}
+          </div>
+          <p className="mt-2 review-card-content">
+            {showMore
+              ? review.content
+              : review.content.substring(0, maxReviewContentLength)}
+            {review.content.length > maxReviewContentLength &&
+              !showMore &&
+              "..."}
+            {review.content.length > maxReviewContentLength && (
+              <button
+                className="review-show-more-button bg-transparent"
+                title={showMore ? "show less" : "show more"}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setShowMore(!showMore);
+                }}
+              >
+                {showMore ? "show less" : "show more"}
+              </button>
+            )}
+          </p>
+          <div className="review-card-stats">
+            <p className="d-inline">
+              <FaHeart className="me-1" />
+              {review.likers.length}
             </p>
-            <div className="review-card-stats">
-              <p className="d-inline">
-                <FaHeart className="me-1" />
-                {review.likers.length}
-              </p>
-              <p className="d-inline ms-4">
-                <FaComment className="me-1" />
-                {review.totalComments}
-              </p>
-              <p className="d-inline float-end mb-0">
-                {`${timeSince(new Date(review.date))} ago`}
-              </p>
-            </div>
+            <p className="d-inline ms-4">
+              <FaComment className="me-1" />
+              {review.totalComments}
+            </p>
+            <p className="d-inline float-end mb-0">
+              {`${timeSince(new Date(review.date))} ago`}
+            </p>
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
