7b3b608fac2c2b3e3dbce61faaf122a823e84e25
Konrad Goldammer <konradgoldammer@gmail.com>
2021-10-05 11:44:53 +0200

    Redesign 'MainFooter'

--- client/src/components/shared/MainFooter.js
+++ client/src/components/shared/MainFooter.js
@@ -1,54 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const MainFooter = () => {
   return (
     <div className="main-footer mt-5">
       <div className="container-md">
         <div className="row">
-          <div className="col-md-8 mt-4 p-0">
-            <div className="row">
-              <div className="col-md-2">
-                <h4 className="footer-brand text-lowercase">Owlery</h4>
-              </div>
-              <div className="col-md-10">
-                <div className="row mt-1">
-                  <div className="col-1 d-flex justify-content-center">
-                    <Link className="footer-nav-link text-decoration-none">
-                      <strong>About</strong>
-                    </Link>
-                  </div>
-                  <div className="col-1 d-flex justify-content-center">
-                    <Link className="footer-nav-link text-decoration-none">
-                      <strong>Help</strong>
-                    </Link>
-                  </div>
-                  <div className="col-1 d-flex justify-content-center">
-                    <Link className="footer-nav-link text-decoration-none">
-                      <strong>Contact</strong>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="col-md-8 mt-4">
+            <h4 className="footer-brand text-lowercase d-inline">Owlery</h4>
+            <Link className="footer-nav-link text-decoration-none ms-4">
+              <strong>About</strong>
+            </Link>
+            <Link className="footer-nav-link text-decoration-none ms-2">
+              <strong>Help</strong>
+            </Link>
+            <Link className="footer-nav-link text-decoration-none ms-2">
+              <strong>Contact</strong>
+            </Link>
             <div className="row">
               <p>&copy; 2021 Owlery. Podcast data from Listen API.</p>
             </div>
           </div>
-          <div className="col-md-4 my-4 p-0">
+          <div className="col-md-4 my-4">
             <div className="float-end">
               <a
                 href="https://instagram.com"
                 className="footer-socials-element"
+                title="Owlery on Instagram"
               >
                 <FaInstagram className="footer-socials-icon" />
               </a>
               <a
                 href="https://twitter.com"
                 className="footer-socials-element ms-2"
+                title="Owlery on Twitter"
               >
                 <FaTwitter className="footer-socials-icon" />
+              </a>
+              <a
+                href="https://youtube.com"
+                className="footer-socials-element ms-2"
+                title="Owlery on YouTube"
+              >
+                <FaYoutube className="footer-socials-icon" />
               </a>
             </div>
           </div>
