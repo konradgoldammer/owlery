abfedba03f15f65494ae12e13925a1bdc12b468d
Konrad Goldammer <konradgoldammer@gmail.com>
2021-10-26 16:08:00 +0200

    Add 'FiveStarRating' component

--- client/src/components/shared/ReviewCard.js
+++ client/src/components/shared/ReviewCard.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
 import { FaUserCircle, FaHeart, FaComment } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { timeSince } from "../../vars";
+import FiveStarRating from "./FiveStarRating";
 
 const ReviewCard = (props) => {
   const { review } = props;
@@ -38,47 +38,7 @@ const ReviewCard = (props) => {
                 {review.author.username}
               </p>
             </Link>
-            {rating !== -1 && (
-              <div className="row d-inline ms-2">
-                <div className="col d-inline p-0">
-                  {rating <= 0 ? (
-                    <BsStar />
-                  ) : rating === 1 ? (
-                    <BsStarHalf />
-                  ) : (
-                    <BsStarFill />
-                  )}
-                  {rating <= 2 ? (
-                    <BsStar />
-                  ) : rating === 3 ? (
-                    <BsStarHalf />
-                  ) : (
-                    <BsStarFill />
-                  )}
-                  {rating <= 4 ? (
-                    <BsStar />
-                  ) : rating === 5 ? (
-                    <BsStarHalf />
-                  ) : (
-                    <BsStarFill />
-                  )}
-                  {rating <= 6 ? (
-                    <BsStar />
-                  ) : rating === 7 ? (
-                    <BsStarHalf />
-                  ) : (
-                    <BsStarFill />
-                  )}
-                  {rating <= 8 ? (
-                    <BsStar />
-                  ) : rating === 9 ? (
-                    <BsStarHalf />
-                  ) : (
-                    <BsStarFill />
-                  )}
-                </div>
-              </div>
-            )}
+            <FiveStarRating rating={rating} />
             {like && (
               <p className="d-inline ms-2">
                 <FaHeart />
